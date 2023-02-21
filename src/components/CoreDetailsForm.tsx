3a882ced09c056d7b31928b938ed2c2c7d5e0da2
Ben-Christie <ben.christie11@gmail.com>
2023-02-21 00:35:50 +0000

    updated user details and added new date picker

--- src/components/CoreDetailsForm.tsx
+++ src/components/CoreDetailsForm.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import FormDropdown from "./FormDropdown";
+import DateOfBirthDropdown from "./DateOfBirthDropdown";
 import SubmitButton from "./SubmitButton";
 import FormInputField from "./FormInputField";
 import AddCoreDetails from "../utilities/requests/AddCoreDetails";
+import AllValuesDefined from "../utilities/AllValuesDefined";
 
 const CoreDetailsForm = () => {
   const [firstName, setFirstName] = useState('');
@@ -11,6 +13,8 @@ const CoreDetailsForm = () => {
   const [gender, setGender] = useState('Male');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [athleteOrCoach, setAthleteOrCoach] = useState('Athlete');
+  const [culprit, setCulprit] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -18,7 +22,14 @@ const CoreDetailsForm = () => {
 
     const response = await AddCoreDetails(firstName, lastName, dateOfBirth, gender, phoneNumber, athleteOrCoach);
 
-    console.log(response);
+    if(!AllValuesDefined(response?.data)) {
+      console.error('Error: response from function AddCoreDetails has undefined value');
+    } else {
+      const responseData = response?.data;
+
+      setCulprit(responseData.culprit);
+      setErrorMessage(responseData.errorMessage);
+    }
   }
 
 
@@ -26,15 +37,16 @@ const CoreDetailsForm = () => {
     <form onSubmit={handleSubmit} className="flex items-center p-2 flex-col h-4/5 w-3/6 rounded-xl bg-z12-gray opacity-70">
       <div className="text-3xl font-bold mt-6 tracking-wide text-white">My Details</div>
       <div className="grid grid-cols-2 grid-rows-3 w-full mt-3 overflow-auto">
-        <FormInputField title="First Name" name="firstname" type="text" changeHandler={setFirstName} paddingTop="pt-5" />
+        
+        <FormInputField title="First Name" name="firstname" type="text" changeHandler={setFirstName} culprit={culprit} errorMessage={errorMessage} paddingTop="pt-5" />
 
-        <FormInputField title="Surname" name="surname" type="text" changeHandler={setLastName} paddingTop="pt-5" />
+        <FormInputField title="Last Name" name="lastname" type="text" changeHandler={setLastName} culprit={culprit} errorMessage={errorMessage} paddingTop="pt-5" />
 
-        <FormInputField title="Date of Birth" name="dob" type="text" placeholder="DD/MM/YYYY" changeHandler={setDateOfBirth} paddingTop="pt-5" />
+        <DateOfBirthDropdown title="Date of Birth" changeHandler={setDateOfBirth} paddingTop="pt-5" />
 
         <FormDropdown title="Gender" options={['Male', 'Female']} value={gender} changeHandler={setGender} paddingTop="pt-5" />
 
-        <FormInputField title="Phone Number" name="phonenumber" type="text" changeHandler={setPhoneNumber} paddingTop="pt-5" paddingBottom="pb-5" />
+        <FormInputField title="Phone Number" name="phonenumber" type="text" changeHandler={setPhoneNumber} culprit={culprit} errorMessage={errorMessage} paddingTop="pt-5" paddingBottom="pb-5" />
 
         <FormDropdown title="Athlete or Coach?" options={['Athlete', 'Coach', 'Both']} value={athleteOrCoach} changeHandler={setAthleteOrCoach} paddingTop="pt-5" paddingBottom="pb-5" />
       </div>
---
+++ src/components/DateOfBirthDropdown.tsx
@@ -0,0 +1,68 @@
+import classNames from "classnames";
+import { useState } from "react";
+
+interface Props {
+  title: string;
+  changeHandler: React.Dispatch<React.SetStateAction<string>>;
+  paddingTop?: string;
+  paddingBottom?: string;
+}
+
+const generatePaddedValues = (min: number, max: number) => {
+  const values = [];
+
+  for(let i = min; i <= max; i++) {
+    const formattedValue = i.toString().padStart(2, '0');
+    values.push(formattedValue);
+  }
+
+  return values;
+}
+
+const generateYears = (present: number, past: number) => {
+  const years = [];
+
+  for(let i = present; i >= past; i--) {
+    years.push(i.toString());
+  }
+
+  return years;
+}
+
+const currentYear = new Date().getFullYear();
+
+const days = generatePaddedValues(1, 31);
+const months = generatePaddedValues(1, 12);
+const years = generateYears(currentYear, (currentYear - 100));
+
+const DateOfBirthDropdown = ({title, changeHandler, paddingTop, paddingBottom}: Props) => {
+  const [day, setDay] = useState('01');
+  const [month, setMonth] = useState('01');
+  const [year, setYear] = useState(currentYear.toString());
+
+  const selectedDate = `${day}/${month}/${year}`;
+
+  changeHandler(selectedDate);
+
+  return (
+    <div className={classNames('flex', 'flex-col', 'px-10', paddingTop, paddingBottom, 'font-bold')}>
+      <label className="text-lg font-bold mb-2 text-orange-400">{title}</label>
+      <div className="grid grid-cols-3">
+        
+        <select onChange={(event: React.ChangeEvent<HTMLSelectElement>) => setDay(event.target.value)} className="p-2.5 rounded-lg text-lg focus:outline-none focus:outline-orange-500 mr-3 text-center">
+          {days.map(day => <option key={day} value={day}>{day}</option>)}
+        </select>
+
+        <select onChange={(event: React.ChangeEvent<HTMLSelectElement>) => setMonth(event.target.value)} className="p-3 rounded-lg text-lg focus:outline-none focus:outline-orange-500 mr-3 text-center">
+          {months.map(month => <option key={month} value={month}>{month}</option>)}
+        </select>
+
+        <select onChange={(event: React.ChangeEvent<HTMLSelectElement>) => setYear(event.target.value)} className="p-2.5 rounded-lg text-lg focus:outline-none focus:outline-orange-500 text-center">
+          {years.map(year => <option key={year} value={year}>{year}</option>)}
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export default DateOfBirthDropdown;
